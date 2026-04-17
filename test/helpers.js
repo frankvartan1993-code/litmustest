f32f380467e303c1fe6689a6c15c95739e08b8db
Carl <frankvartan1993@gmail.com>
2026-04-17 12:07:32 -0400

    test: shared fixture loader and rule runner helpers

---
+++ test/helpers.js
@@ -0,0 +1,18 @@
+'use strict';
+const fs = require('node:fs');
+const path = require('node:path');
+const parse = require('../lib/parse');
+
+function loadFixture(name) {
+  return fs.readFileSync(path.join(__dirname, '..', 'fixtures', name), 'utf8');
+}
+
+async function runRule(rule, html) {
+  const ctx = parse(html);
+  if (rule.detect) rule.detect(ctx);
+  if (rule.fix) rule.fix(ctx);
+  ctx.macros.after = require('../lib/parse').captureMacros(ctx.html);
+  return ctx;
+}
+
+module.exports = { loadFixture, runRule };
